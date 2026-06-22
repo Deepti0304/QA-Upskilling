4a73aeab6fd31469e4b1f18fca7a9e5cb507a926
deepti-patel0304 <deeptimayee22@gmail.com>
2026-06-22 15:29:10 +0530

    update Day1.py file

--- Day1.py
+++ Day1.py
@@ -1,1 +1,16 @@
 print ("hello world- Deepti")
+# #
+# Features of Python
+# 1.Open Source
+# 2.License Free
+# 3.Avaiable for Free
+# 4.Easy to Learn
+# 5.Dynamically Typed - ex- a=10, a="Deepti", a=10.5
+# 6.Interpreted Language - code is executed line by line
+# 7.Procedure Oriented Programming Language - ex- function, procedure, method
+# 8.Object Oriented - Abstraction, Encapsulation, Inheritance, Polymorphism
+# 9.Extensible - can be extended to other languages like C, C++
+# 10.Where we use Python - Web Development, Data Science, Machine Learning, 
+# Artificial Intelligence, Automation, Scripting, Game Development, etc.
+# #
+
